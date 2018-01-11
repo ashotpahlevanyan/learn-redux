facf9ae0a8639094965d3a8361c55fd786f89299
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-01-11 23:53:26 +0400

    committing the es6 structure for state, we can use just comments instead of comments: comment in state object

---
+++ learn-redux/store.js
@@ -0,0 +1,16 @@
+import { createStore, compse } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+//import the root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+//create an object for the default data
+
+const defaultState = {
+	posts: posts,
+	comments: comments
+};
