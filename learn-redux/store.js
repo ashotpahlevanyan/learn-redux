74f148949abc2e02a72cd774e348e9eddb6bd799
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-01-12 00:11:00 +0400

    added action creators

--- learn-redux/store.js
+++ learn-redux/store.js
@@ -11,6 +11,10 @@ import posts from './data/posts';
 //create an object for the default data
 
 const defaultState = {
-	posts: posts,
-	comments: comments
+	posts,
+	comments
 };
+
+export default const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
